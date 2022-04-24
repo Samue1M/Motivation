35ae9f00c3b7b2313cddae789cd486ceecaf93ba
Samuel Marinho <88913689+Samue1M@users.noreply.github.com>
2022-04-24 20:25:39 -0300

    Update main,py

--- main.py
+++ main.py
@@ -18,8 +18,14 @@ Frase1 = f"\nTudo o que um sonho precisa para ser realizado é alguém que acred
 Frase2 = f"\nA persistência é o caminho do êxito.\n";
 Frase3 = f"\nÉ parte da cura o desejo de ser curado.\n";
 Frase4 = f"\nNo meio da dificuldade encontra-se a oportunidade.\n";
+Frase5 = f"\nAs pessoas costumam dizer que a motivação não dura sempre. Bem, nem o efeito do banho, por isso recomenda-se diariamente.\n";
+Frase6 = f"\nMotivação é a arte de fazer as pessoas fazerem o que você quer que elas façam porque elas o querem fazer.\n";
+Frase7 = f"\nToda ação humana, quer se torne positiva ou negativa, precisa depender de motivação.\n";
+Frase8 = f"\nLute. Acredite. Conquiste. Perca. Deseje. Espere. Alcance. Invada. Caia. Seja tudo o quiser ser, mas, acima de tudo, seja você sempre.\n";
+Frase9 = f"\nA verdadeira motivação vem de realização, desenvolvimento pessoal, satisfação no trabalho e reconhecimento.\n";
+Frase10 = f"\nImagine uma nova história para sua vida e acredite nela.\n";
 
-y = random.choice([Frase1,Frase2,Frase3,Frase4]);
+y = random.choice([Frase1,Frase2,Frase3,Frase4,Frase5,Frase6,Frase7,Frase8,Frase9,Frase10]);
 
 if(Selector == "Sim"):
     print(y);
